1a5b437ffabc6bdda6c57ec54225a1b532a19ada
sohel902833 <mdsohrabhossainsohel902802@gmail.com>
2022-12-26 11:32:50 +0600

    get by role assertaion practices

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Greet from "./components/Greet";
+import Greet from "./components/greet/Greet";
+import Application from "./components/application/Application";
 
 function App() {
   return (
     <div className="App">
       <Greet />
+      <Application />
     </div>
   );
 }
